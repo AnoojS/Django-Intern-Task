6a4de7801fe629391c6600c3a9bcd3af4b87f5a7
AnoojS <anooj10000@gmail.com>
2023-02-24 11:12:20 +0530

    New Commit

--- app1_manager/views.py
+++ app1_manager/views.py
@@ -4,9 +4,16 @@ from rest_framework.response import Response
 
 from .models import Post
 from .serializers import PostSerializer
+from .forms import PostForm
 
 def simple_page(request):
-    return render(request,'simple-page.html')
+    form=PostForm()
+    if request.method =='POST':
+        form=PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+    context={'form':form}
+    return render(request,'simple-page.html',context)
 
 class simple_api(viewsets.ModelViewSet):
     queryset=Post.objects.all()
